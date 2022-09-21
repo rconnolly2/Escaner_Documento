dccfce29dd3d8062883ce7bb3a52d6305bc34fd2
Robert Connolly <robertoconnolly100@gmail.com>
2022-09-21 14:32:33 +0200

    Commit 1

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import cv2
 import numpy as np
 
+webcam = cv2.VideoCapture(0)
+
+while(webcam.isOpened()):
+
+    _, fotograma = webcam.read()
+    cv2.imshow("Webcam", fotograma)
+    waitkey = cv2.waitKey(20)
+
+    #Si presionamos la tecla "q" el programa se cierra
+    if waitkey == ord("q"):
+        break
+
+
+cv2.destroyAllWindows()
